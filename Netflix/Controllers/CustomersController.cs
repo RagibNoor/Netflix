7739b32bb9655b7236c6d2295389669c488e561b
RagibNoor <ragibnoor19@gmail.com>
2018-11-03 16:57:34 +0600

    setup done

---
+++ Netflix/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Netflix.Models;
+
+namespace Netflix.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //
+        // GET: /Customers/
+        public ActionResult Index()
+        {
+            var customer = GetCustomers();
+            return View(customer);
+        }
+
+        public ActionResult Details(int id)
+        {
+            string name = null;
+            int flag = 0;
+
+            var customer = GetCustomers().Find(x => x.Id == id);
+            if (customer!=null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+        public List<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "C1", Id = 1},
+                new Customer{Name = "C2" , Id = 2}
+            };
+
+            return customers;
+        } 
+	}
+}
---
+++ Netflix/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Netflix.Models;
+using Netflix.ViewModel;
+
+namespace Netflix.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movies/
+        public ActionResult Random()
+        {
+            var movie =new  Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "C1"},
+                new Customer{Name = "C2"}
+            };
+
+            var viewModel = new  RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex , string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("PageIndex= {0}& sortBy = {1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,30)}")]
+        public ActionResult ByReleaseYear(int year , int month)
+        {
+            return Content(year + "/" + month);
+        }
+	}
+}
